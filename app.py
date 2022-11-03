8919d35c61b14c84e3185722d9e556d8710a1411
asya-code <77514066+asya-code@users.noreply.github.com>
2022-11-02 19:41:53 -0700

    voter_by_adress function added to calculate the participation rate for University of Pennsylvania blockchain student organization

--- app.py
+++ app.py
@@ -33,21 +33,34 @@ def full_voters_data(skip_entry):
         skip_entry += 20000
     return voters_dict
 
-def m_active_part_rate(voter, votes_amount):
+def participation_rate(voter, votes_amount):
     participation = 100 / votes_amount * voter[1]
     return voter[0], participation
 
+
+def voter_by_adress(address):
+    #since we know from the full_voters_data results that the max amount of votes by one address is less than 20000 where is no need to change this part of the query
+    body = f'{{ votes(first: 20000, where: {{space: "aave.eth",  voter: "0x070341aA5Ed571f0FB2c4a5641409B1A46b4961b"}}, orderBy: "created", orderDirection: asc) {{voter}} }}'
+    response = requests.get(url=url, json={"query": body})
+    print(response)
+    votes = response.json()["data"]["votes"]
+    voter = (address, len(votes))
+    return voter
+
+
 full_voters_data(skip_entry)
-#print(voters_dict['0x7A3BdeE62dd34faC317Ae61cd8B3bA7c27ada145'])
 
 sorted_voters = sorted(voters_dict.items(), key=operator.itemgetter(1), reverse=True)
 raw_most_active_20 = sorted_voters[0:20]
-#print(raw_most_active_20)
 most_active_20 = [voter[0] for voter in raw_most_active_20]
-part_rate_20 = [m_active_part_rate(voter, votes_amount) for voter in raw_most_active_20]
+
+part_rate_20 = [participation_rate(voter, votes_amount) for voter in raw_most_active_20]
+
+#University of Pennsylvania blockchain student organization
+up_address = "0x070341aA5Ed571f0FB2c4a5641409B1A46b4961b"
+up_participation = participation_rate(voter_by_adress(up_address), votes_amount)
 
 print(datetime.datetime.now())
-print(most_active_20)
-print(votes_amount)
 print(part_rate_20)
+print(up_participation)
 
